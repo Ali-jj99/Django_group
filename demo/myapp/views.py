6edd6a279de4f439efdb941ce4752df3ce817905
Kelyan <darrelkelyan@outlook.com>
2025-04-09 22:00:42 +0100

    Fixed django

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "team_summary.html")
+
+def login_view(request):
+    return render(request, "log_in.html")
