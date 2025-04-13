a083d29c52b1259914c4b6d891d49cc6ba008a72
Kelyan <darrelkelyan@outlook.com>
2025-04-13 14:07:57 +0100

    front-end for sign up page, need to add the sky logo and incorppoted some javascript.

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class Vote(models.Model):
-    voteID = models.IntegerField()
-    userID = models.IntegerField()
-    healthCardID = models.IntegerField()
-    sessionID = models.IntegerField()
-    voteColour = models.CharField(max_length=5)
-    ProgressNote = models.CharField(max_length=255)
-    Timestamp = models.DateTimeField( db_comment="Date and time when the user voted",)
+    voteID = models.IntegerField(primary_key=True)
+    userID = models.ForeignKey('User', on_delete=models.CASCADE, db_column='userID')
+    healthCardID = models.ForeignKey('HealthCard', on_delete=models.CASCADE, db_column='healthCardID')
+    sessionID = models.ForeignKey('Session', on_delete=models.CASCADE, db_column='sessionID')
+    progressNote = models.TextField(null=False, blank=True)
+    timestamp = models.DateTimeField()
+    voteValue = models.CharField(max_length=6, null=False,
+                                 choices = [
+                                     ('Green', 'Green'),
+                                     ('Amber', 'Amber'),
+                                     ('Red', 'Red')
+                                 ])
     
+#Created table for Team
 class Team(models.Model):
-    teamID = models.Field.primary_key=True
-    Name = models.CharField(max_length=30)
-    departmentID = models.IntegerField()
+    teamID = models.IntegerField(primary_key=True)
+    name = models.TextField(null=False)
     teamLeadID = models.IntegerField()
+    userID = models.ForeignKey('User', on_delete=models.CASCADE, db_column='userID')
+    healthCardID = models.ForeignKey('HealthCard', on_delete=models.CASCADE, db_column='healthCardID')
+    departmentID = models.ForeignKey('Department', on_delete=models.CASCADE, db_column='departmentID')
