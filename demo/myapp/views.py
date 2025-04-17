255fc497fb8d6796e5231c5e6eb36107ad4954c0
Kelyan <darrelkelyan@outlook.com>
2025-04-17 14:15:28 +0100

    Added css to django correctly and implemented universal navbar

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 #imports the database into the website
-from .models import Vote
+from .models import Vote, Team, Department, Session, User
 
 # Create your views here.
 def home(request):
     #this passes the Vote table into the html page
     all_votes = Vote.objects.all
-    return render(request, "team_summary.html", {'vote' : all_votes})
+    return render(request, "landing_page.html", {'vote' : all_votes})
+
+def join(request):
+    return render(request, "join.html", {})
 
 def login_view(request):
     return render(request, "log_in.html")
 
-def join(request):
-    return render(request, "join.html", {})
+def vote_view(request):
+    return render(request, "voting_page.html")
+
+def progress_view(request):
+    return render(request, "progress_page.html")
+
+def signup_view(request):
+    return render(request, "sign_up.html")
+
+def summary_view(request):
+    return render(request, "team_summary.html")
+
+
