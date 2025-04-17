878a68b1d8ee6a6a9d9e8a2a706ee44cdb931639
Kelyan <darrelkelyan@outlook.com>
2025-04-17 13:07:48 +0100

    Made required changes

--- demo/myapp/admin.py
+++ demo/myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Vote, Team
+from .models import Vote, Team, Department, Session, User
 #this adds the database to the admin page 
 
 admin.site.register(Vote)
 admin.site.register(Team)
+admin.site.register(Department)
+admin.site.register(Session)
+admin.site.register(User)
